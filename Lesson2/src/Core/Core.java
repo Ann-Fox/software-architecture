b5a4b4bf5bbc91361d1802c9106e75f53459558b
Ann-Fox <fx8565@mail.ru>
2023-10-06 16:49:41 +0500

    generator

--- Lesson2/src/Core/Core.java
+++ Lesson2/src/Core/Core.java
@@ -1,18 +1,32 @@
 package Core;
 
 import Abstraction.ItemGenerator;
+import Rewards.Brass.BrassGenerator;
 import Rewards.Gold.GoldGenerator;
+import Rewards.Silver.SilverGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Core {
 
-    public void run() {
+     public void run() {
         List<ItemGenerator> generatorList = new ArrayList<>();
         generatorList.add(new GoldGenerator());
+        generatorList.add(new SilverGenerator());
+        generatorList.add(new BrassGenerator());
         // generatorList.add(...)
-        generatorList[...];
 
+        int randomGeneratorNumber = GetRandomInt(generatorList.size());
+
+        ItemGenerator randomGenerator = generatorList.get(randomGeneratorNumber);
+        String reward = randomGenerator.openReward();
+        System.out.println(reward);
+    }
+
+    public int GetRandomInt(int maxValue) {
+        int randomNumber = ThreadLocalRandom.current().nextInt(0, maxValue);
+        return randomNumber;
     }
 }
