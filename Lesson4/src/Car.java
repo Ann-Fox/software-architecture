8369939efa138a70078bb19e2d105bec40941a99
Ann-Fox <fx8565@mail.ru>
2023-10-19 15:41:45 +0500

    characteristics

--- Lesson4/src/Car.java
+++ Lesson4/src/Car.java
@@ -1,21 +1,26 @@
 public abstract class Car {
+    // марка автомобиля
     String brand;
+    // модель автомобиля
     String model;
+    // количество колес
     int weels;
-    String fuelType; // TODO: make enum
+    // тип топлива (бензин, дизель, газ)
+    Fuel fuelType; // TODO: make enum - перечисление, когда есть фиксированные значения (выпадающий список)
+    // тип трансмиссии (автоматические, механические)
     Transmition transmitionType;
 
-    Car(String brand, String model, Transmition transmitionType, int numWeels, String fuelType) {
+    Car(String brand, String model, Transmition transmitionType, int weels, Fuel fuelType) {
         this.brand = brand;
         this.model = model;
         this.transmitionType = transmitionType;
         this.weels = weels;
         this.fuelType = fuelType;
     }
-
+// движение
     abstract void move();
-
+// сервис
     abstract void service();
-
+// вкл/выкл фонарей
     abstract void turnOnHeadlights();
 }
